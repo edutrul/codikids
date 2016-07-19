770d83de12c8a1b7a49b3a871264ae9a04b2003e
BenjiSantos <santsben@gmail.com>
2016-07-19 14:20:18 -0500

    content in the center

--- index.php
+++ index.php
@@ -18,23 +18,28 @@ $conn->close();
       
 <!-- Page Features -->
   <div class="row grid">
-    <?php foreach ($juegos as $juego):?>
-      <div class="element-item col-md-2 <?php print $juego->dificultad ?>" style="width:100%;"><br>
-            <img src="images/juegos/<?php print $juego->imagen; ?>" alt="angrybird juego" style="position:relative; padding-left:20px" width="350px">
-              <div style="float:right; text-align:center; padding-right:50%;"><h2 class="nombre"><?php print $juego->titulo; ?></h2>
+              <?php foreach ($juegos as $juego):?>
+        <div class="element-item col-md-2 <?php print $juego->dificultad ?>" style="width:100%;text-align:center;">
               <br>
-              <span class="descripcion"><?php print $juego->descripcion; ?></span><br>
-                <a href="/reporte.php?<?php print $juego->juego_id; ?>" target="_blank">Ver reporte</a><br><br><br>
-                <span class="descripcion"> Tiempo transcurrido</span><br>
-                      <div class="progress">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                              5m 27s
-                            </div>
-                     </div>
-   
-</div>
-      </div>
-    <?php endforeach; ?>
+              <img src="images/juegos/<?php print $juego->imagen; ?>" alt="angrybird juego" style="position:relative; padding-left:20px" width="300px">
+                  <div style="float:right;  padding-right:20%; ">
+                        <div style="padding:8px; width:300px;">
+                            <h2 class="nombre"><?php print $juego->titulo;?></h2>
+                              <br>
+                                <span class="descripcion"><?php print $juego->descripcion; ?></span><br>
+                                    <a href="/reporte.php?<?php print $juego->juego_id; ?>" target="_blank">Ver reporte</a><br>
+                                        <br><br>
+                                           <span class="descripcion"> Tiempo transcurrido</span><br>
+                                  <div class="progress">
+                                              <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                                5m 27s
+                        </div>                      
+                                              </div>
+                                  </div>
+         
+                  </div>
+         </div>
+              <?php endforeach; ?>
   </div>
 <!-- /.row -->
 <?php require_once "footer.php"; ?>
