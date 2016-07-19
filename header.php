76266f40cb522874826eebf49f0853bef0c72774
BenjiSantos <santsben@gmail.com>
2016-07-19 15:02:43 -0500

    not uppercase font in the user

--- header.php
+++ header.php
@@ -33,11 +33,11 @@
                                           <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span><span
                                               class="icon-bar"></span><span class="icon-bar"></span>
                                       </button>
-                                    <a class="navbar-brand" href="#">Bienvenido: <?php print strtoupper($username); ?></a>
+                                    <a class="navbar-brand" href="#">Bienvenido: <?php print $username; ?></a>
                                   </div>
                                   <div class="navbar-collapse collapse">
                                       <ul class="nav navbar-nav">
-                                          <li class="active"><a href="/users/dashboard">Dashboard</a></li>
+                                          <li class="active"><a href="/users/dashboard">Reportes</a></li>
                                       </ul>
                                       <ul class="nav navbar-nav navbar-right">
                                           <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php print $username; ?>
